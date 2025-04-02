ef43b034e80c176140ed8a853f31a7d4ea76a2b7
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2025-04-02 22:18:21 +0530

    Update SecurityConfig.java

--- SecurityConfig.java
+++ SecurityConfig.java
@@ -1,13 +1,29 @@
 package com.example.notes.security;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.notes.models.AppRole;
+import com.example.notes.models.Role;
+import com.example.notes.models.User;
+import com.example.notes.repositories.RoleRepository;
+import com.example.notes.repositories.UserRepository;
+
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.time.LocalDate;
+
+import javax.sql.DataSource;
+
 	
 @Configuration
 @EnableWebSecurity
@@ -24,4 +40,65 @@ public class SecurityConfig {
 		http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();
 	}
+  /*  @Bean
+    public UserDetailsService userDetailsService(DataSource ds) {
+        JdbcUserDetailsManager manager =
+                new JdbcUserDetailsManager(ds);
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
+    }*/
+	@Bean
+    public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
+        return args -> {
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER)));
+
+            Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
+
+            if (!userRepository.existsByUserName("user1")) {
+                User user1 = new User("user1", "user1@example.com", "{noop}password1");
+                user1.setAccountNonLocked(false);
+                user1.setAccountNonExpired(true);
+                user1.setCredentialsNonExpired(true);
+                user1.setEnabled(true);
+                user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                user1.setTwoFactorEnabled(false);
+                user1.setSignUpMethod("email");
+                user1.setRole(userRole);
+                userRepository.save(user1);
+            }
+            
+            if (!userRepository.existsByUserName("admin")) {
+                User admin = new User("admin", "admin@example.com", "{noop}adminPass");
+                admin.setAccountNonLocked(true);
+                admin.setAccountNonExpired(true);
+                admin.setCredentialsNonExpired(true);
+                admin.setEnabled(true);
+                admin.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                admin.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                admin.setTwoFactorEnabled(false);
+                admin.setSignUpMethod("email");
+                admin.setRole(adminRole);
+                userRepository.save(admin);
+            }
+        };
+    }
+
 }
