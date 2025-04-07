971d0fb20227c8178395361c8e60da6c8253a8ad
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2025-04-07 22:27:15 +0530

    Create AdminController.java

---
+++ AdminController.java
@@ -0,0 +1,38 @@
+package com.example.notes.controller;
+
+import com.example.notes.dtos.UserDTO;
+import com.example.notes.models.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.notes.service.UserService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @PutMapping("/update-role")
+    public ResponseEntity<String> updateUserRole(@RequestParam Long userId, 
+                                                 @RequestParam String roleName) {
+        userService.updateUserRole(userId, roleName);
+        return ResponseEntity.ok("User role updated");
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    }
+
+
+}
